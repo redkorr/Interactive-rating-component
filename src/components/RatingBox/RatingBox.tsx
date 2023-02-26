841fb651d41078058d6f258b2a1d05829c00fed9
redkorr <redkor910.97@gmail.com>
2023-02-26 16:38:41 +0100

    created RatingBox component

--- src/components/RatingBox/RatingBox.tsx
+++ src/components/RatingBox/RatingBox.tsx
@@ -1,20 +1,39 @@
-import React from 'react';
+import React, { useState } from 'react';
+
 import styles from './RatingBox.module.css';
 import RatingButtons from '../RatingButtons/RatingButtons';
-import SubmitButton from '../SubmitButton/SubmitButton';
+import iconStar from '../../assets/icon-star.svg';
+import ThankYouState from './ThankYouState/ThankYouState';
 
-function RatingBox() {
+function RatingBox(): React.ReactElement {
+  const [index, setIndex] = useState<number>(0);
+  const [step, setStep] = useState<number>(0);
+
+  const handleSubmit = () => {
+    setStep((step) => step + 1);
+  };
   return (
-    <div className={styles.ratingBox}>
-      <img className={styles.ratingIcon} src='/src/assets/icon-star.svg'></img>
-      <p className={styles.header}>How did we do?</p>
-      <p className={styles.text}>
-        Please let us know how we did with your support request. All feedback is
-        appreciated to help us improve our offering
-      </p>
-      <RatingBox />
-      <SubmitButton />
-    </div>
+    <>
+      {step === 0 && (
+        <div className={styles.ratingBox}>
+          <img className={styles.ratingIcon} src={iconStar}></img>
+          <p className={styles.ratingBoxHeader}>How did we do?</p>
+          <p className={styles.ratingBoxText}>
+            Please let us know how we did with your support request. All
+            feedback is appreciated to help us improve our offering
+          </p>
+          <RatingButtons onChange={setIndex} selectedIndex={index} />
+          <button
+            className={styles.submitButton}
+            onClick={handleSubmit}
+            disabled={index === 0}
+          >
+            SUBMIT
+          </button>
+        </div>
+      )}
+      {step === 1 && <ThankYouState index={index} />}
+    </>
   );
 }
 
